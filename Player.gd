c241c8f7cde00ee947c4bbfb88f6f9848cc56096
filpatterson <filpatterson10@gmail.com>
2022-02-19 19:23:55 +0200

    velocity via acceleration and inertia, code refactor, small optimization, gate lines bug fix

--- Player.gd
+++ Player.gd
@@ -1,14 +1,13 @@
 extends KinematicBody
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# movement constants
+const MAX_SPEED = 15
+const ACCELERATION = 1.3
+const INERTION = 1
 
-# constants used in movement function
-const SPEED = 10
-const GRAVITY = 0.98
-const ROTATE = 0.01
-var y_pos = 0
+# rotation speed and velocity vector
+const ROTATION_SPEED = 0.01
+var velocity = Vector3()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,38 +18,53 @@ func _ready():
 #	pass
 
 # function performs control over the player at each frame
-
 func _physics_process(delta):
-	# initalization of 3d movement vector and changing value (speed) depending on pressed buttons
-	var movement_vec = Vector3()
+	# setting acceleration vector, required to estimate acceleration of player
+	var acceleration_vec = Vector3()
+	
 	if Input.is_action_pressed("move_forward"):
-		movement_vec.z -= 1
+		acceleration_vec.z -= 1
 	if Input.is_action_pressed("move_backward"):
-		movement_vec.z += 1
+		acceleration_vec.z += 1
 		
-	# considering that project implements car movement and it is not possible to move left/right
-	# instantly here is applied rotation of the object. Considering that car is not able to turn
-	# if there is no velocity of the vehicle it is required to set rotation only in case of car
-	# movement. 
 	if Input.is_action_pressed("move_right"):
-		rotate_y(-ROTATE)
+		rotate_y(-ROTATION_SPEED)
 	if Input.is_action_pressed("move_left"):
-		rotate_y(ROTATE)
+		rotate_y(ROTATION_SPEED)
 	
-	# perform speed normalization and perform speed multiplication to speed up the vehicle
-	movement_vec = movement_vec.normalized()
-	movement_vec *= SPEED
-	movement_vec.y = 0
-
-	move_and_slide(transform.basis.xform(Vector3(0, y_pos, movement_vec.z))) 
+	# perform vector normalization (evading errors of too many signals from button received)
+	acceleration_vec = acceleration_vec.normalized()
 	
-	y_pos -= GRAVITY
-	print(delta)
+	# considering that player controls a car it is required to set a model that will scale 
+	# acceleration depending on the speed (higher speed is - lower acceleration is). Currently
+	# works with 4 levels of acceleration, can be changed in future
+	if velocity.z > MAX_SPEED / 2 || velocity.z < -MAX_SPEED / 2:
+		acceleration_vec *= ACCELERATION * 0.9
+		if velocity.z > MAX_SPEED / 1.4 || velocity.z < -MAX_SPEED / 1.4:
+			acceleration_vec *= ACCELERATION * 0.9
+			if velocity.z > MAX_SPEED / 1.1 || velocity.z < -MAX_SPEED / 1.1:
+				acceleration_vec *= ACCELERATION * 0.95
+	else:
+		acceleration_vec *= ACCELERATION
+	
+	velocity.z += acceleration_vec.z
+	
+	# if car moves, then its acceleration is reduced by inertion
+	if velocity.z > 0:
+		velocity.z -= INERTION
+	if velocity.z < 0:
+		velocity.z += INERTION
 		
+	# car is unable to move higher than specified speed limit. Another option to reach the same
+	# result is total acceleration removal at stage of getting highest speed
+	if velocity.z > MAX_SPEED:
+		velocity.z = MAX_SPEED
+	elif velocity.z < -MAX_SPEED:
+		velocity.z = -MAX_SPEED
 
-func manually_handle_collision():
-	var hitcount = get_slide_count()	
-	if hitcount > 0:
-		var collision = get_slide_collision(0)
-		if collision.collider is RigidBody:
-			collision.collider.apply_impulse(collision.position, - collision.normal)
+	# apply velocity vector to the method responsible for movement
+	move_and_slide(transform.basis.xform(Vector3(0, 0, velocity.z))) 
+	
+	# for debug
+	print(velocity.z)
+		
